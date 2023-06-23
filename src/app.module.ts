ecdaf1ca33d3fc09b49ca4b95d3302ee15135701
Dimaint <shmik.v@dreamlab.kz>
2023-06-23 14:36:33 +0600

    remove test "articles" module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { ArticlesModule } from './articles/articles.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [
-    PrismaModule,
-    ArticlesModule,
-    AuthModule,
-    UsersModule,
-    EmployeeModule,
-  ],
+  imports: [PrismaModule, AuthModule, UsersModule, EmployeeModule],
   controllers: [AppController],
   providers: [AppService],
 })
